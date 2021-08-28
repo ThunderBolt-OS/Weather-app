d06bc432e0bf56ed8b554e5174562f95c477d9a6
Soham5102 <soham.bhoir@somaiya.edu>
2021-08-28 14:35:41 +0530

    my first commit to this app

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require('express')
 const PORT = process.env.PORT || 5000
 const app = express()
 
+//* importing routing files
+const WheatherRoute = require('./Routes/wheather.js')
+
+//* using template engine as ejs
+app.set('view engine', 'ejs')
+
+//* middlware router
+app.use('/', Wheather)
 
 app.listen(PORT, () => {
     console.log(`ThunderBolt's server is running on https://localhost:${PORT}`)
- })
+})
