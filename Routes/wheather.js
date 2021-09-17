8d2eca0f5e7ad3c6e0256c4f737010a73141b510
Soham5102 <soham.bhoir@somaiya.edu>
2021-09-17 16:07:04 +0530

    new update ti api data

--- Routes/wheather.js
+++ Routes/wheather.js
@@ -1,11 +1,32 @@
-const router = require('express').Router()
+  
+const router = require('express').Router();
+const axios = require('axios');
+// ?const fetch = require('node-fetch');
+require('dotenv').config()
 
 router.get('/', (req, res) => {
     res.render('index')
 })
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     console.log(req.body)
+    
+    const city = req.body.city;
+    console.log(city);
+    const url_api = `https://api.openweathermap.org/data/2.5/weather?q=`+ city +`&appid=f8479523969c89afd226b37fa696839e`
+    console.log(url_api)
+
+    try {
+        axios.get(url_api).then(resp => {
+
+            console.log(resp.data);
+            // consol
+         });
+        
+    } catch(err) {
+        
+    }
+
 })
 
 module.exports = router;
