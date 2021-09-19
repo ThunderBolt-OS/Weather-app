0fca5a3d1833c4ddbd286287a78e6586fe1f44dc
Soham5102 <soham.bhoir@somaiya.edu>
2021-09-20 01:29:38 +0530

    project done

--- Routes/wheather.js
+++ Routes/wheather.js
@@ -4,78 +4,108 @@ const axios = require('axios');
 require('dotenv').config()
 
 router.get('/', (req, res) => {
-    res.render('index', (req, res) => {
-        city = null;
-        des = null;
-        icon = null;
-        temp = null;
-        feels_like = null;
-        pressure = null;
-        humidity = null;
-        wind_speed = null;
+    res.render('index', {
+        city: null,
+        des: null,
+        icon: null,
+        temp: null,
+        feels_like: null,
+        pressure: null,
+        humidity: null,
+        wind_speed: null,
 
     });
+    console.log("hello world 5")
 });
 
 router.post('/', async (req, res) => {
     console.log(req.body)
     
     const city = req.body.city;
-    console.log(city);
+    // console.log(city);
     const url_api = `https://api.openweathermap.org/data/2.5/weather?q=` + city + `&appid=f8479523969c89afd226b37fa696839e`
     console.log(url_api)
+    // console.log("hello world 7")
 
-    try {
-        await axios.get(url_api).then(resp => {
 
-            console.log(resp.data);
+    
+    axios.get(url_api).then(resp => {
 
-            if (data.message === 'city not found') {
-                res.render('index', () => {
+        console.log(resp.data);
 
-                    city: data.message;
-                    des: null;
-                    feels_like: null;
-                    humidity: null;
-                    pressure: null;
-                    temp: null;
-                    icon: null;
-                    wind: null;
-                });
+        if (resp.data.message === 'city not found') {
+            res.render('index',  {
 
-            } else {
-
-                const city = data.name;
-                const des = data.weather[0].description;
-                const icon = data.weather[0].icon;
-                const temp = data.main.temp;
-                const feels_like = data.main.feels_like;
-                const pressure = data.main.pressure;
-                const humidity = data.main.humidity;
-                const wind_speed = data.wind.speed;
-
-                res.render('index', () => {
-                    city, des, icon, temp, feels_like, pressure, humidity, wind_speed
-                });
-                
-            }
+                city: resp.data.message,
+                des: null,
+                feels_like: null,
+                humidity: null,
+                pressure: null,
+                temp: null,
+                icon: null,
+                wind: null,
             
+            });
+
+
+    }   else {
+
+        const city = resp.data.name;
+        const des = resp.data.weather[0].description;
+        const icon = resp.data.weather[0].icon;
+        const temp = resp.data.main.temp;
+        const feels_like = resp.data.main.feels_like;
+        const pressure = resp.data.main.pressure;
+        const humidity = resp.data.main.humidity;
+        const wind_speed = resp.data.wind.speed;
+
+        res.render('index', {
+            city: city,
+            des: des,
+            icon:icon,
+            temp: temp,
+            feels_like: feels_like,
+            pressure: pressure,
+            humidity: humidity,
+            wind_speed: wind_speed,
         });
         
-    } catch (err) {
-        
-        city = 'something is wrong (ﾉ◕ヮ◕)';
-        des = null;
-        icon = null;
-        temp = null;
-        feels_like = null;
-        pressure = null;
-        humidity = null;
-        wind_speed = null;
-
         
     }
+            
+        }).catch((err) => {
+             
+        console.log(err)
+        let city = 'something is wrong (ﾉ◕ヮ◕)';
+        let des = null;
+        let icon = null;
+        let temp = null;
+        let feels_like = null;
+        let pressure = null;
+        let humidity = null;
+        let wind_speed = null;
+
+        res.render('index', {
+            city: city,
+            des: des,
+            icon:icon,
+            temp: temp,
+            feels_like: feels_like,
+            pressure: pressure,
+            humidity: humidity,
+            wind_speed: wind_speed,
+        });
+        
+
+            
+        })
+        
+    
+    // console.log("hello world 9")
+
 
 });
+// console.log("hello world 10")
+
 
 module.exports = router;
--- app.js
+++ app.js
@@ -5,15 +5,23 @@ const app = express();
 //* Middelware -->
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+console.log("hello world middleware")
+
 
 //* importing routing files -->
 const WheatherRoute = require('./Routes/wheather.js');
+console.log("hello world routing")
+
 
 //* using template engine as ejs -->
 app.set('view engine', 'ejs');
+console.log("hello world engine")
+
 
 //* middlware router -->
 app.use('/', WheatherRoute);
+console.log("hello world middle router")
+
 
 app.listen(PORT, () => {
     console.log(`ThunderBolt's server is running on http://localhost:${PORT}`)
